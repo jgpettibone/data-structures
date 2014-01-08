41f5940ee54c0d7f675c2dc4e855bea08bf1d156
jgpettibone <jeanette.pettibone@gmail.com>
2014-01-08 13:30:22 -0800

    added the functional pattern queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var first = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size+first] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    if (size) {
+      size--;
+      var result = storage[first];
+      delete storage[first];
+      first++;
+    }
+
+    return result;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,28 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
-  /* START PROMPT
-    var size; // Hint: set an initial value here
-  -END PROMPT */
+  var storage = {};
+  var size = 0;
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    if (size) {
+      size--;
+      var result = storage[size];
+      delete storage[size];
+    }
+    return result;
   };
 
   instance.size = function(){
+    return size;
   };
   
-    return instance;
-  };
+  return instance;
+};

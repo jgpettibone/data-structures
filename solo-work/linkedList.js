fc9c6eca0c732231c549449d5e4d8cfc2c98ab5e
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:34:33 -0700

    implemented a linked list

--- solo-work/linkedList.js
+++ solo-work/linkedList.js
@@ -4,12 +4,30 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if (this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    }
+    else {
+      this.tail.next = node;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    if (this.head)
+      this.head = this.head.next;      
   };
 
   list.contains = function(target, node){
+    if (this.tail === null) return false;
+    var current = this.head;
+    while (current) {
+      if (current.value === target) return true;
+      current = current.next;
+    }
+    return false;
   };
 
   return list;

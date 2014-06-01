8a217ff49d7d5a693b6cd941e19f6f246ae31590
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:33:24 -0700

    implemented a queue in the prototypal instantiation pattern

--- solo-work/prototypal/queue.js
+++ solo-work/prototypal/queue.js
@@ -1,5 +1,26 @@
 var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+  instance._storage = {};
+  instance._last = 0;
+  instance._first = 0;
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this._storage[this._last] = value;
+  this._last++;
+};
+
+queueMethods.dequeue = function() {
+  var result = this._storage[this._first];
+  this._first++;
+  return result;
+};
+
+queueMethods.size = function() {
+  if (this._last - this._first >= 0)
+    return this._last - this._first;
+  return 0;
+};

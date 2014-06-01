8f6ade9267da2b64f768ef810c612c5ea9b1b191
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:35:01 -0700

    implemeneted a set

--- solo-work/set.js
+++ solo-work/set.js
@@ -1,16 +1,21 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  if (this._storage[item]) return true;
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if (this._storage[item])
+    delete this._storage[item];
 };

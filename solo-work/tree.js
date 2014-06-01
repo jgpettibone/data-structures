da5364d17635c52cf1fb5b4641f203f698d526e0
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 20:46:24 -0700

    implemented a removeFromParent method on tree

--- solo-work/tree.js
+++ solo-work/tree.js
@@ -1,11 +1,12 @@
 var Tree = function(value){
   this.value = value;
+  this.parent = null;
   this.children = [];
 };
 
-
 Tree.prototype.addChild = function(value){
   var child = new Tree(value);
+  child.parent = this;
   this.children.push(child);
 };
 
@@ -19,3 +20,8 @@ Tree.prototype.contains = function(target){
   return false;
 };
 
+Tree.prototype.removeFromParent = function(){
+  var index = this.parent.children.indexOf(this);
+  this.parent.children = this.parent.children.slice(0,index) + this.parent.children.slice(index+1);
+  this.parent = null;
+};

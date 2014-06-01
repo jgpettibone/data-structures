5a15305b8a14488b04ad8b8eba00dff9b771803d
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 20:22:25 -0700

    renamed graph privates with underscore

--- solo-work/graph.js
+++ solo-work/graph.js
@@ -1,46 +1,46 @@
 var Graph = function(){
-  this.nodes = {};
-  this.edges = {};
+  this._nodes = {};
+  this._edges = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
-  if (Object.keys(this.nodes).length === 1) 
-    this.addEdge(Object.keys(this.nodes)[0], newNode);
+  if (Object.keys(this._nodes).length === 1) 
+    this.addEdge(Object.keys(this._nodes)[0], newNode);
   if (toNode)
     this.addEdge(newNode, toNode);
-  this.nodes[newNode] = true;
+  this._nodes[newNode] = true;
 };
 
 Graph.prototype.contains = function(node){
-  if (this.nodes[node]) return true;
+  if (this._nodes[node]) return true;
   return false;
 };
 
 Graph.prototype.removeNode = function(node){
-  if (this.nodes[node])
-    delete this.nodes[node];
+  if (this._nodes[node])
+    delete this._nodes[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
-  if (this.edges[fromNode] === toNode) return true;
+  if (this._edges[fromNode] === toNode) return true;
   return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  this.edges[fromNode] = toNode;
+  this._edges[fromNode] = toNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  if (this.edges[fromNode] === toNode)
-    delete this.edges[fromNode];    
+  if (this._edges[fromNode] === toNode)
+    delete this._edges[fromNode];    
   if (!this.hasEdge(fromNode)) this.removeNode(fromNode);
   if (!this.hasEdge(toNode)) this.removeNode(toNode);
 };
 
 Graph.prototype.hasEdge = function(node){
-  for (var key in this.edges) {
+  for (var key in this._edges) {
     if (key === node) return true;
-    if (this.edges[key] === node) return true; 
+    if (this._edges[key] === node) return true; 
   }
   return false;
 };

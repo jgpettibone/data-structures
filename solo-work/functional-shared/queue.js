95fc5e9bb6db136d46b45487491a7f987957054d
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:32:44 -0700

    implemented a queue in the functional-shared instantiation pattern

--- solo-work/functional-shared/queue.js
+++ solo-work/functional-shared/queue.js
@@ -1,5 +1,28 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  instance._storage = {};
+  instance._last = 0;
+  instance._first = 0;
+
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this._storage[this._last] = value;
+  this._last++;
+};
+
+queueMethods.dequeue = function() {
+  var result = this._storage[this._first];
+  this._first++;
+  return result;
+};
+
+queueMethods.size = function() {
+  if (this._last - this._first >= 0)
+    return this._last - this._first;
+  return 0;
+};

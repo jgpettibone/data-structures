4af4e77aa54791678d8b979e570aaa4e245a11f2
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 21:34:54 -0700

    implemented resizing of hash table

--- solo-work/hashTable.js
+++ solo-work/hashTable.js
@@ -1,23 +1,39 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  if (bucket) 
-    bucket.push([k,v]);
-  else 
+  if (bucket) {
+    var found = false;
+    for (var j = 0; j < bucket.length; j++) {
+      if (bucket[j][0] === k) {
+        bucket[j][1] = v;
+        found = true;
+      }
+    }
+    if (!found)
+      bucket.push([k,v]);
+  }
+  else {
     bucket = [[k,v]];
+  }
   this._storage.set(i, bucket);
+  if (++this._size > this._limit * .75) {
+    this.resize(this._limit*2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  for (var j = 0; j < bucket.length; j++) {
-    if (bucket[j][0] === k) return bucket[j][1];
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) return bucket[i][1];
+    }
   }
   return null;
 };
@@ -25,12 +41,31 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  for (var j = 0; j < bucket.length; j++) {
-    if (bucket[j][0] === k) {
-      bucket = bucket.slice(0,j) + bucket.slice(j+1); 
+  if (bucket) {
+    for (var j = 0; j < bucket.length; j++) {
+      if (bucket[j][0] === k) {
+        bucket = bucket.slice(0,j).concat(bucket.slice(j+1));
+      }
     }
   }
   this._storage.set(i,bucket);
+  if (--this._size < this._limit * .25) {
+    this.resize(this._limit/2);    
+  }
 
+};
+
+HashTable.prototype.resize = function(newLimit){
+  var storage = this._storage;
+  this._limit = newLimit;
+  this._storage = makeLimitedArray(this._limit);
+  for (var i = 0; i < storage.length; i++) {
+    if (storage[i]){
+      var bucket = storage[i];
+      for (var j = 0; j < bucket.length; j++){
+        this.insert(bucket[j][0], bucket[j][1]);
+      }
+    }
+  }
 };
 

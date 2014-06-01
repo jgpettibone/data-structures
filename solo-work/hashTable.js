b220fa43ee445f5f8e1e84356411ccbb0c32b696
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:47:39 -0700

    implemented basic hash table

--- solo-work/hashTable.js
+++ solo-work/hashTable.js
@@ -5,12 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if (bucket) 
+    bucket.push([k,v]);
+  else 
+    bucket = [[k,v]];
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) return bucket[j][1];
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket = bucket.slice(0,j) + bucket.slice(j+1); 
+    }
+  }
+  this._storage.set(i,bucket);
+
 };
 

481043c30ff6942a404e25b5a63ae2afdc9df1a1
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-31 19:34:03 -0700

    implemented a queue in the pseudoclassical instantiation pattern

--- solo-work/pseudoclassical/queue.js
+++ solo-work/pseudoclassical/queue.js
@@ -1,3 +1,22 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this._storage = {};
+  this._last = 0;
+  this._first = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._last] = value;
+  this._last++;
+};
+
+Queue.prototype.dequeue = function() {
+  var result = this._storage[this._first];
+  this._first++;
+  return result;
+};
+
+Queue.prototype.size = function() {
+  if (this._last - this._first >=0)
+    return this._last - this._first;
+  return 0;
 };
